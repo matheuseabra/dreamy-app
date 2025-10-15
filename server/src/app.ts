7bbc0a18daf782f3f4e03eca4e716346832c90b3
matheus <matheusvieiracoelho@gmail.com>
2025-10-14 23:35:55 -0300

    update env

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,7 +20,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:9000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:8080',
     credentials: true,
   })
 );
